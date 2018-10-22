6510b8b7b87576b3d2aa756d30a2f6ea58f57295
algon-320 <algon.0320@gmail.com>
2018-10-22 19:39:27 +0900

    TypeScriptでやっていくことにした

--- ts/graph-vis.ts
+++ ts/graph-vis.ts
@@ -1,30 +1,53 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas");
+const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
 canvas.width = 1800;
 canvas.height = 1000;
 ctx.font = "bold 16px 'Ubuntu Mono'";
 
-var adjList = document.getElementById("adjacencylist");
-var adjMat = document.getElementById("adjacencymat");
-var vertexRadiusInput = document.getElementById("vertexRadiusInput");
-var edgeLengthInput = document.getElementById("edgeLengthInput");
+const adjList: HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById("adjacencylist");
+const adjMat: HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById("adjacencymat");
+const vertexRadiusInput: HTMLInputElement = <HTMLInputElement>document.getElementById("vertexRadiusInput");
+const edgeLengthInput: HTMLInputElement = <HTMLInputElement>document.getElementById("edgeLengthInput");
 
 let vertexRadius = 20;
 let edgeLength = 200;
-vertexRadiusInput.value = vertexRadius;
-edgeLengthInput.value = edgeLength;
+vertexRadiusInput.value = vertexRadius.toString();
+edgeLengthInput.value = edgeLength.toString();
 
+class Vec {
+    constructor(public x: number, public y: number) { }
+    static inv(v1: Vec): Vec {
+        return { x: -v1.x, y: -v1.y };
+    }
+    static add(v1: Vec, v2: Vec): Vec {
+        return { x: v1.x + v2.x, y: v1.y + v2.y };
+    }
+    static sub(v1: Vec, v2: Vec): Vec {
+        return { x: v1.x - v2.x, y: v1.y - v2.y };
+    }
+    static scalar(v1: Vec, sc: number): Vec {
+        return { x: v1.x * sc, y: v1.y * sc };
+    }
+    static abs(v1: Vec): number {
+        return Math.sqrt(v1.x * v1.x + v1.y * v1.y);
+    }
+}
+type Point = Vec;
 
-// ベクトル演算関数
-function vec_inv(v1) { return { x: -v1.x, y: -v1.y }; }
-function vec_add(v1, v2) { return { x: v1.x + v2.x, y: v1.y + v2.y }; }
-function vec_sub(v1, v2) { return vec_add(v1, vec_inv(v2)); }
-function vec_scalar(v1, sc) { return { x: v1.x * sc, y: v1.y * sc }; }
-function vec_abs(v1) { return Math.sqrt(v1.x * v1.x + v1.y * v1.y); }
+class Vertex {
+    p: Point;
+    v: Vec;
+    fixed: boolean;
+    constructor() {
+        this.p = new Vec(Math.random() * 1000, Math.random() * 1000);
+        this.v = new Vec(0, 0);
+        this.fixed = false;
+    }
+}
 
-var numVertex = 6;
-var vs = [];
-var edge = [
+let numVertex = 6;
+let vs: Array<Vertex> = [];
+let edge: Array<Array<number>> = [
     [0, 1, 0, 0, 1, 0],
     [1, 0, 1, 0, 1, 0],
     [0, 1, 0, 1, 0, 0],
@@ -32,26 +55,21 @@ var edge = [
     [1, 1, 0, 1, 0, 0],
     [0, 0, 0, 1, 0, 0],
 ];
-var clicked = -1;
-
-function addVertex() {
-    vs.push({ v: { x: 0, y: 0 }, p: { x: Math.random() * 1000, y: Math.random() * 1000 }, fixed: false });
-}
-
+let clicked: number = -1;
 
 // 描画関係
-let edgeColor = "rgb(30, 30, 30)";
-let vertexColorClicked = "rgb(70, 70, 255)";
-let vertexColorNormal = "rgb(255, 70, 70)";
-let fontColor = "white";
-function draw() {
+let edgeColor: string = "rgb(30, 30, 30)";
+let vertexColorClicked: string = "rgb(70, 70, 255)";
+let vertexColorNormal: string = "rgb(255, 70, 70)";
+let fontColor: string = "white";
+function draw(): void {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // 辺
     ctx.strokeStyle = edgeColor;
     ctx.lineWidth = 3;
     for (let i = 0; i < numVertex; i++) {
-        for (let j = i + 1; j < numVertex; j++) {
+        for (let j: number = i + 1; j < numVertex; j++) {
             if (edge[i][j] == 0) continue;
             ctx.beginPath();
             ctx.moveTo(vs[i].p.x, vs[i].p.y);
@@ -79,34 +97,32 @@ function draw() {
     }
 }
 
-function initiFromTextarea(changeAdjList) {
+function initiFromTextarea(isAdjList: boolean): void {
     // console.log("textarea changed");
-    if (changeAdjList) {
+    if (isAdjList) {
         // 隣接リスト
         let mxID = -1;
         let lines = adjList.value.split(/\r\n|\r|\n/);
-        let tmp = [];
+        let tmp: Array<[number, number]> = [];
         for (let i = 0; i < lines.length; i++) {
             if (lines[i] === "") continue;
             let e = lines[i].split(/\s*,\s*|\s+/);
             if (e.length < 2 || e[1] === "") continue;
             let a = parseInt(e[0]), b = parseInt(e[1]);
-            if (mxID < a) mxID = a;
-            if (mxID < b) mxID = b;
+            mxID = Math.max(mxID, a, b);
             tmp.push([a, b]);
         }
-        let newNumVertex = mxID + 1;
+        let newNumVertex: number = mxID + 1;
         edge = [];
         for (let i = 0; i < newNumVertex; i++) {
-            edge.push(new Array(newNumVertex));
+            edge.push(new Array<number>(newNumVertex));
             for (let j = 0; j < newNumVertex; j++) {
                 edge[i][j] = 0;
             }
         }
 
-        for (let i = numVertex; i <= newNumVertex; i++) addVertex();
+        for (let i = numVertex; i <= newNumVertex; i++) vs.push(new Vertex());
         numVertex = newNumVertex;
-        console.log(numVertex);
 
         for (let i = 0; i < tmp.length; i++) {
             let a = tmp[i][0], b = tmp[i][1];
@@ -114,12 +130,12 @@ function initiFromTextarea(changeAdjList) {
         }
     } else {
         let invalid = false;
-        lines = adjMat.value.split(/\r\n|\r|\n/);
+        let lines = adjMat.value.split(/\r\n|\r|\n/);
         let cols = -1;
-        let mat = [];
+        let mat: Array<Array<number>> = [];
         for (let i = 0; i < lines.length; i++) {
             if (lines[i] === "") continue;
-            let row = lines[i].split(/\s*,\s*|\s+/);
+            let row = lines[i].split(/\s*,\s*|\s+/).map(x => parseInt(x));
             mat.push(row);
             if (cols == -1) cols = row.length;
             else if (cols != row.length) {
@@ -128,7 +144,7 @@ function initiFromTextarea(changeAdjList) {
         }
         if (!invalid) {
             let newNumVertex = cols;
-            for (let i = numVertex; i <= newNumVertex; i++) addVertex();
+            for (let i = numVertex; i <= newNumVertex; i++) vs.push(new Vertex());
             edge = mat;
             numVertex = cols;
         }
@@ -138,18 +154,18 @@ function initiFromTextarea(changeAdjList) {
 adjList.addEventListener("change", function () { initiFromTextarea(true) }, false);
 adjMat.addEventListener("change", function () { initiFromTextarea(false) }, false);
 
-function demoInit() {
+function demoInit(): void {
     for (let i = 0; i < numVertex; i++) {
-        vs.push({ v: { x: 0, y: 0 }, p: { x: Math.random() * 1000, y: Math.random() * 1000 }, fixed: false });
+        vs.push(new Vertex());
     }
-    vs[0].p = { x: 500, y: 500 };
+    vs[0].p = new Vec(500, 500);
     vs[0].fixed = true;
 }
 
 let sumEnergy = 0;
-function update() {
+function update(): void {
     // textareaの更新
-    var valueAL = "";
+    let valueAL = "";
     for (let i = 0; i < numVertex; i++) {
         for (let j = i; j < numVertex; j++) {
             if (edge[i][j] == 1) {
@@ -158,7 +174,8 @@ function update() {
         }
     }
     adjList.value = valueAL;
-    var valueAM = "";
+
+    let valueAM = "";
     for (let i = 0; i < numVertex; i++) {
         let row = "";
         for (let j = 0; j < numVertex; j++) {
@@ -171,54 +188,51 @@ function update() {
     adjMat.value = valueAM;
 
     // 定数 ------------------------
-    let coulombK = 1000;
-    let springK = 1;
-    let energyLowerbound = 0.1;
-    let deltaT = 0.1;
-    let M = 1;
-    let decK = 0.95;
+    const coulombK = 1000;
+    const springK = 1;
+    const energyLowerbound = 0.1;
+    const deltaT = 0.1;
+    const M = 1;
+    const decK = 0.95;
     // -----------------------------
-    function f() {
+    function f(): void {
         sumEnergy = 0;
         for (let i = 0; i < numVertex; i++) {
-            let v1 = vs[i];
+            const v1 = vs[i];
 
-            let F = { x: 0, y: 0 };
+            let F = new Vec(0, 0);
 
             // クーロン力
             for (let j = 0; j < numVertex; j++) {
                 if (i == j) continue;
-                let v2 = vs[j];
+                const v2 = vs[j];
 
-                let vec = vec_sub(v1.p, v2.p);
-                let absf = coulombK / vec_abs(vec);
+                let vec = Vec.sub(v1.p, v2.p);
+                let absf = coulombK / Vec.abs(vec);
                 absf = absf * absf;
 
                 let f = vec;
-                f = vec_scalar(f, absf / vec_abs(f));  // 長さを設定
-                F = vec_add(F, f);
+                f = Vec.scalar(f, absf / Vec.abs(f));  // 長さを設定
+                F = Vec.add(F, f);
             }
-            // console.log("c F = ", F);
 
             // フックの法則
             for (let j = 0; j < numVertex; j++) {
                 if (edge[i][j] == 0) continue;
-                let v2 = vs[j];
-                let vec = vec_sub(v2.p, v1.p);
+                const v2 = vs[j];
+                let vec = Vec.sub(v2.p, v1.p);
 
-                let absf = springK * (vec_abs(vec) - edgeLength);
+                let absf = springK * (Vec.abs(vec) - edgeLength);
                 let f = vec;
-                f = vec_scalar(f, absf / vec_abs(f));  // 長さを設定
-                F = vec_add(F, f);
+                f = Vec.scalar(f, absf / Vec.abs(f));  // 長さを設定
+                F = Vec.add(F, f);
             }
 
-            // console.log("f F = ", F);
             if (!v1.fixed) {
-                v1.v = vec_scalar(vec_add(v1.v, vec_scalar(F, deltaT / M)), decK);
-                v1.p = vec_add(v1.p, vec_scalar(v1.v, deltaT));
+                v1.v = Vec.scalar(Vec.add(v1.v, Vec.scalar(F, deltaT / M)), decK);
+                v1.p = Vec.add(v1.p, Vec.scalar(v1.v, deltaT));
             }
-            // console.log(v1.v);
-            sumEnergy += M * vec_abs(v1.v) * vec_abs(v1.v);
+            sumEnergy += M * Vec.abs(v1.v) * Vec.abs(v1.v);
         }
         draw();
         // console.log("sumEnergy: ", sumEnergy);
@@ -233,23 +247,23 @@ function update() {
     if (sumEnergy <= energyLowerbound) f();
 }
 
-function onMouseDown(e) {
+function onMouseDown(e: MouseEvent): void {
     // console.log("clicked = ", clicked, e.offsetY, e.offsetX);
     for (let i = 0; i < numVertex; i++) {
-        if (vec_abs(vec_sub(vs[i].p, { x: e.offsetX, y: e.offsetY })) < vertexRadius) {
+        if (Vec.abs(Vec.sub(vs[i].p, { x: e.offsetX, y: e.offsetY })) < vertexRadius) {
             clicked = i;
             break;
         }
     }
     draw();
 }
-function onMouseUp(e) {
+function onMouseUp(e: MouseEvent): void {
     if (clicked == -1) return;
     if (clicked != 0) vs[clicked].fixed = false;
     clicked = -1;
     draw();
 }
-function onMouseMove(e) {
+function onMouseMove(e: MouseEvent): void {
     if (clicked == -1) return;
     vs[clicked].p = { x: e.offsetX, y: e.offsetY };
     vs[clicked].fixed = true;
@@ -259,9 +273,9 @@ canvas.addEventListener("mousedown", onMouseDown, false);
 canvas.addEventListener("mouseup", onMouseUp, false);
 canvas.addEventListener("mousemove", onMouseMove, false);
 
-function onUIupdated() {
-    vertexRadius = vertexRadiusInput.value;
-    edgeLength = edgeLengthInput.value;
+function onUIupdated(): void {
+    vertexRadius = parseInt(vertexRadiusInput.value);
+    edgeLength = parseInt(edgeLengthInput.value);
     update();
 }
 vertexRadiusInput.addEventListener("change", onUIupdated, false);
